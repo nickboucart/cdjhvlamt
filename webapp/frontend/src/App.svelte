c8a711f1bc18f2b7f4404a181e04086286c3a08e
Nick Boucart <nboucart@gmail.com>
2022-11-15 15:12:34 +0100

    introductie v backend en deploy stuff

---
+++ webapp/frontend/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { Router, Link, Route } from "svelte-routing";
+  import Home from "./routes/Home.svelte";
+  import Vlammetjes from "./routes/Vlammetjes.svelte";
+  import Vlam from "./routes/Vlam.svelte";
+  
+  export let url = "";
+  </script>
+  
+  
+  <Router url="{url}">
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/vlammetjes">De Vlammetjes</Link>
+      <!-- <Link to="about">About</Link>
+      <Link to="blog">Blog</Link> -->
+    </nav>
+    <div>
+      <!-- <Route path="blog/:id" component="{BlogPost}" />
+      <Route path="blog" component="{Blog}" />
+      <Route path="about" component="{About}" /> -->
+      <Route path="vlammetjes/:id" component="{Vlam}" />
+      <Route path="vlammetjes" component="{Vlammetjes}" />
+      <Route path="/"><Home /></Route>
+    </div>
+  </Router>
---
+++ webapp/stacks/MyStack.js
@@ -0,0 +1,27 @@
+import { Api, ViteStaticSite } from "@serverless-stack/resources";
+
+export function MyStack({ stack }) {
+  const api = new Api(stack, "api", {
+    routes: {
+      "GET /": "functions/lambda.handler",
+    },
+  });
+
+
+// Deploy our Svelte app
+const site = new ViteStaticSite(stack, "SvelteJSSite", {
+  path: "frontend",
+  environment: {
+    // Pass in the API endpoint to our app
+    VITE_APP_API_URL: api.url,
+  },
+});
+
+// Show the URLs in the output
+stack.addOutputs({
+  SiteUrl: site.url,
+  ApiEndpoint: api.url,
+});
+
+
+}
