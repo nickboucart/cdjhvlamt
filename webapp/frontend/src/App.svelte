ddd7c8661bb37fb9b3ae4a08c69207013483ca0f
Nick Boucart <nboucart@gmail.com>
2022-11-20 22:33:09 +0100

    beetje opkuis

--- webapp/frontend/src/App.svelte
+++ webapp/frontend/src/App.svelte
@@ -4,13 +4,10 @@
 
 
   import Home from "./routes/Home.svelte";
-  import Vlammetjes from "./routes/Vlammetjes.svelte";
   import Vlam from "./routes/Vlam.svelte";
   import OverOns from "./routes/OverOns.svelte";
   import Footer from "./components/Footer.svelte";
   import Privacy from "./routes/Privacy.svelte";
-
-  // import VlamMap from "./components/VlamMap.svelte";
   
   export let url = "";
   const queryClient = new QueryClient();
@@ -20,12 +17,10 @@
   <Router url="{url}">
     <nav>
       <Link to="/">Home</Link>
-      <Link to="/vlammetjes">De Vlammetjes</Link>
       <Link to="/about">Over Ons</Link>
     </nav>
     <div>
       <Route path="vlammetjes/:id" component="{Vlam}" />
-      <Route path="vlammetjes" component="{Vlammetjes}" />
       <Route path="about" component="{OverOns}" />
       <Route path="privacy" component="{Privacy}" />
       <Route path="/"><Home /></Route>
--- webapp/frontend/src/routes/Vlam.svelte
+++ webapp/frontend/src/routes/Vlam.svelte
@@ -1,17 +1,13 @@
 <script>
 	import { useQuery } from "@sveltestack/svelte-query";
 
-	import { getData, postData, getDataForQuery } from "../helpers/APIHelpers";
+	import { postData, getDataForQuery } from "../helpers/APIHelpers";
 
-	let vlam;
-
-	const queryResult = useQuery("getVlam" + $$props.id, () =>
-		getDataForQuery(("/vlammeke/" + $$props.id)), {staleTime: 3*60*1000});
-
-	// async function getVlam() {
-	// 	vlam = await getData("/vlammeke/" + $$props.id);
-	// 	return vlam;
-	// }
+	const queryResult = useQuery(
+		"getVlam" + $$props.id,
+		() => getDataForQuery("/vlammeke/" + $$props.id),
+		{ staleTime: 3 * 60 * 1000 }
+	);
 
 	async function onClick(thingName, animatie) {
 		await postData("/vlammekes/" + thingName, { animatie: animatie });
@@ -22,17 +18,22 @@
 	<h1>Individuele Vlam</h1>
 
 	{#if $queryResult.isLoading}
-<span>Loading...</span>
-{:else if $queryResult.error}
-<span>An error has occurred: {$queryResult.error.message}</span>
-{:else}
-<h2>Toon de details van {$queryResult.data.attributes.eigenaar}</h2>
-<p>
-	Stuur het vlammetje aan:
-	<button on:click={() => onClick($queryResult.data.thingName, "fire")}>vlam animatie</button>
-	<button on:click={() => onClick($queryResult.data.thingName, "conjunction")}>andere animatie</button>
-</p>
-{/if}
+		<span>Loading...</span>
+	{:else if $queryResult.error}
+		<span>An error has occurred: {$queryResult.error.message}</span>
+	{:else}
+		<h2>Toon de details van {$queryResult.data.attributes.eigenaar}</h2>
+		<p>
+			Stuur het vlammetje aan:
+			<button on:click={() => onClick($queryResult.data.thingName, "fire")}
+				>vlam animatie</button
+			>
+			<button
+				on:click={() => onClick($queryResult.data.thingName, "conjunction")}
+				>andere animatie</button
+			>
+		</p>
+	{/if}
 	<!-- {#await getVlam()}
 		Even geduld, we laden data...
 	{:then vlam}
@@ -43,6 +44,4 @@
 			<button on:click={() => onClick("conjunction")}>andere animatie</button>
 		</p>
 	{/await} -->
-
-
 </div>
