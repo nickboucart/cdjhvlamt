6d353dd4cae18cb4cefda8a30cc1e36e8f4e1375
Nick Boucart <nboucart@gmail.com>
2022-11-23 10:24:28 +0100

    navigatie beter gestyled

--- webapp/frontend/src/routes/Home.svelte
+++ webapp/frontend/src/routes/Home.svelte
@@ -3,5 +3,11 @@
 
 </script>
 
+<div class="marketing">
+<h1>Welkom bij CDJHVlamt - Een interactief stadsspel voor de feestdagen!</h1>
+<h2>Zoek de vlammetjes, scan de QR code en unlock de besturing het vlammetje</h2>
+</div>
+<p>
 
-<h1>todo, mooie landing page maken</h1>
+
+</p>
