d8d8ab874f3c2bd3216b007c734c8655726eb364
Nick Boucart <nboucart@gmail.com>
2022-12-10 17:34:17 +0100

    firmware wat aangepast

--- firmware/main.py
+++ firmware/main.py
@@ -21,31 +21,36 @@ try:
 except ImportError:
     import asyncio
 
-
-def main():
-    d = display.Display()
-    mgr = wifimgr.ConnectionManager(d)
+def setupWifiAndMQTT(display, animations, dingNaam):
+    mgr = wifimgr.ConnectionManager(display)
     c = mgr.get_connection()
     ssid = mgr.get_active_ssid()
     passwd = mgr.get_active_password()
     mgr = None
-    c = None
-#     del wifimgr
-#     del sys.modules["wifimgr"]
     gc.collect()
+    if ssid:
+        client = cdjh_mqtt.CDJHVlamtMQTTClient(dingNaam, ssid, passwd, animations)
+        client.run()
+        print('alles runt!!')
+    else:
+        d.text("Oei, we geraken niet op de wifi!!")
+
+
+
+def main():
+    d = display.Display()
     aniMgr = animaties.AnimatieMgr(33, 30)
     aniMgr.stop_annimation_and_run_new_one("fire")
     def handleButtonPress(pin):
         aniMgr.stop()
     button = machine.Pin(0, mode = machine.Pin.IN)
     button.irq(trigger=machine.Pin.IRQ_RISING, handler=handleButtonPress)
-    if ssid:
-        client = cdjh_mqtt.CDJHVlamtMQTTClient(thingName, ssid, passwd, aniMgr)
-        client.run()
-        print('alles runt!!')
-    else:
-        d.text("Oei, we geraken niet op de wifi!!")
-    
+    setupWifiAndMQTT(d, aniMgr, thingName)
+    while True:
+        asyncio.sleep(5)
 
+#     asyncio.create_task(setupWifiAndMQTT(d, aniMgr, thingName))
+#     while 1:
+#         await asyncio.sleep(5)
 
 main()
